221b620a871477266b22b34647061eba81f31ba8
Thanh Le <lenguyenthanh@hotmail.com>
2016-03-14 21:34:56 +0700

    Remove old classes from data and domain module

--- domain/src/main/java/com/lenguyenthanh/snowball/domain/video/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lenguyenthanh.snowball.domain.video;
-
-import com.lenguyenthanh.snowball.domain.Video;
-import java.util.List;
-import rx.Observable;
-
-public interface VideoRepository {
-  Observable<List<Video>> videos();
-}
