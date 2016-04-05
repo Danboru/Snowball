c8904884979a375e16a7ebe33fdd67421c039bce
Thanh Le <lenguyenthanh@hotmail.com>
2016-04-05 14:46:37 +0700

    Refactor test

--- app/src/integrationTests/java/com/lenguyenthanh/snowball/helper/StreamUtil.java
+++ app/src/integrationTests/java/com/lenguyenthanh/snowball/helper/StreamUtil.java
@@ -1,4 +1,15 @@
 package com.lenguyenthanh.snowball.helper;
 
+import java.io.IOException;
+import org.apache.commons.io.IOUtils;
+
 public class StreamUtil {
+  private StreamUtil() {
+    // I know what I'm doing
+  }
+
+  public static String getResponseData(Class<?> clazz, final String filename)
+      throws IOException {
+    return IOUtils.toString(clazz.getResourceAsStream("/response/" + filename));
+  }
 }
